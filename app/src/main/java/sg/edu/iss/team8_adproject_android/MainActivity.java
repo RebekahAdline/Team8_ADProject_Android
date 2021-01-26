b0ac350ae6e153ccda645055d82c083bab767af7
Sankalp <sink257@hotmail.com>
2021-01-26 17:38:00 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/iss/team8_adproject_android/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.iss.team8_adproject_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/sg/edu/iss/team8_adproject_android/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package sg.edu.iss.team8_adproject_android;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
